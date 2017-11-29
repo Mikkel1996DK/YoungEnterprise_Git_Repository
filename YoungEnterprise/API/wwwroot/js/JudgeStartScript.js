98a7eda02b051e03a9cfb7e7a03090e5646cd97a
Luluise <louisebc@gmail.com>
2017-11-29 13:20:38 +0100

    Query Changes

--- YoungEnterprise/API/wwwroot/js/JudgeStartScript.js
+++ YoungEnterprise/API/wwwroot/js/JudgeStartScript.js
@@ -1,22 +1,22 @@
-﻿
-window.onload(alert("Hello"));
+﻿$(document).ready(function () {
 
-//$(document).ready(function () {
+    $('#teamButton').click(function () {
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:53112/api/TblTeams",
+            contentType: "application/json"
+        }).then(function (data) {
+            alert("Hello");
+                $('#teams').empty(); // Clear the table body.
 
-//    $('#teamButton').click(function () {
-//        alert("Hello");
-//        $.getJSON("http://localhost:53112/api/TblTeams",
-//            function (data) {
-//                $('#teams').empty(); // Clear the table body.
-
-//                // Loop through the list of teams.
-//                $.each(data, function (key, value) {
-//                    // Add a table row for each team.
-//                    var row = '<td>' + value.fld_TeamName + '</td><td>' + value.fld_SubjectCategory + '</td><td>' + <button Give Grade /> + '</td>';
-//                    $('<tr/>', { text: row })  // Append the name.
-//                        .appendTo($('#teams'));
-//                });
-//            });
-//    })
-//});
+                // Loop through the list of teams.
+                $.each(data, function (key, value) {
+                    // Add a table row for each team.
+                    var row = '<td>' + value.fld_TeamName + '</td><td>' + value.fld_SubjectCategory + '</td><td>' + <button Give Grade /> + '</td>';
+                    $('<tr/>', { text: row })  // Append the name.
+                        .appendTo($('#teams'));
+                });
+            });
+    })
+});
 
