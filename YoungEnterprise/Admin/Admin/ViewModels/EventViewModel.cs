38015d94da306a4fd6a4c19b36ed548b5a139595
Mikkel1996DK <Mikkelljungberg@gmail.com>
2017-12-06 14:15:55 +0100

    Added working event page.

--- YoungEnterprise/Admin/Admin/ViewModels/EventViewModel.cs
+++ YoungEnterprise/Admin/Admin/ViewModels/EventViewModel.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Service;
+using Service.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using YoungEnterprise_API.Models;
 
 namespace Admin.ViewModels
@@ -12,14 +15,69 @@ namespace Admin.ViewModels
     public class EventViewModel : INotifyCollectionChanged
     {
         public event NotifyCollectionChangedEventHandler CollectionChanged;
-        private ObservableCollection<TblJudgePair> judgePairs = new ObservableCollection<TblJudgePair>();
-        public ObservableCollection<TblJudgePair> JudgePairs
+
+        private DatabaseService dbService = new DatabaseService();
+        private UserService userService = new UserService();
+
+        private ObservableCollection<DisplayJudgePair> judgePairs = new ObservableCollection<DisplayJudgePair>();
+        public ObservableCollection<DisplayJudgePair> JudgePairs
         {
             get { return judgePairs; }
             set { judgePairs = value; }
         }
 
 
+        public EventViewModel()
+        {
+            RefreshCollections();
+        }
+
+        // Used for the "Lav Dommerpar" button.
+        // 1: Shift to judge datagrid
+        // 2: Use the method in Userservice.
+        // 3: Update datagrid
+        public void CreateJudgePairs ()
+        {
+            userService.CreateJudgePairs();
+
+        }
+
+        public void RefreshCollections ()
+        {
+            judgePairs.Clear();
+
+            List<TblJudgePair> pairs = dbService.GetAllJudgePairs();
+            List<TblJudge> judges = dbService.GetAllJudges();
+
+            foreach (TblJudgePair pair in pairs)
+            {
+                DisplayJudgePair displayPair = new DisplayJudgePair();
+                displayPair.JudgePairID = pair.FldJudgePairId;
+
+                displayPair.JudgeNameA = judges.Find(n => n.FldJudgeId == pair.FldJudgeIda).FldJudgeName;
+                displayPair.JudgeNameB = judges.Find(n => n.FldJudgeId == pair.FldJudgeIdb).FldJudgeName;
+
+                judgePairs.Add(displayPair);
+            }
+
+            // Initialize ScheduleCollection here when completed!.
+        }
+
+
+        // Prompt for ok/cancel with a warning
+        // Clear the database
+        // Add a new event with the date from the datepicker
+        public void NewEvent (DateTime dateTime)
+        {
+            string text = "Dette sletter alt indehold i databasen!" + Environment.NewLine + "Vil du fortsætte?";
+            MessageBoxResult result = MessageBox.Show(text, "Advarsel!", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                dbService.DeleteAllRecords();
+                dbService.CreateEvent(dateTime);
+            }
+        }
 
     }
 }
--- YoungEnterprise/Admin/Admin/Views/EventView.xaml.cs
+++ YoungEnterprise/Admin/Admin/Views/EventView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Admin.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,48 @@ namespace Admin.Views
     /// </summary>
     public partial class EventView : UserControl
     {
+        private EventViewModel model = new EventViewModel();
+
         public EventView()
         {
             InitializeComponent();
+
+            ShiftDatagrid(judgePairsDataGrid);
+
+            DataContext = model;
+        }
+
+        private void ShiftDatagrid (DataGrid dataGrid)
+        {
+            viewGrid.Children.Clear();
+            viewGrid.Children.Add(dataGrid);
+        }
+
+        private void createJudgePairsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShiftDatagrid(judgePairsDataGrid);
+            model.CreateJudgePairs();
+            model.RefreshCollections();
+        }
+
+        private void createSchedule_Click(object sender, RoutedEventArgs e)
+        {
+            ShiftDatagrid(scheduleDataGrid);
+            
+            // Make the schedule dynamically here!
+
+            model.RefreshCollections();
+        }
+
+        private void createEventButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (eventDate.SelectedDate != null)
+            {
+                model.NewEvent((DateTime)eventDate.SelectedDate);
+            } else
+            {
+                MessageBox.Show("Indtast en gyldig dato!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
