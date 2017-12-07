7e501550a568c2c7b83c0ec4613b55eee18a4434
Mikkel1996DK <Mikkelljungberg@gmail.com>
2017-12-07 13:30:26 +0100

    Added a model for school

---
+++ YoungEnterprise/Service/Models/TeamsForSchoolModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Models
+{
+    public class TeamsForSchoolModel
+    {
+        public string SchoolUsername { get; set; }
+    }
+}
