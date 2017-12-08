7933171e266dfe65f0357ac1e68365ce05e37a44
Mikkel1996DK <Mikkelljungberg@gmail.com>
2017-12-08 10:15:50 +0100

    CreateTeamScript needs more work due to report upload

--- YoungEnterprise/API/wwwroot/js/CreateTeamScript.js
+++ YoungEnterprise/API/wwwroot/js/CreateTeamScript.js
@@ -9,27 +9,47 @@
         document.getElementById('userNameField').innerHTML = userName;
     });
 
+        /*
+        $('#table').bootstrapTable({
+             data: data
+        });
+        */
+
     $(function GetSchoolsCreateTeam() {
         $.ajax({
             type: 'GET',
             url: 'http://localhost:53112/api/TblSchools',
             contentType: 'application/json',
             success: function (data) {
+                var identifier;
+
                 for (i = 0; i < data.length; i++) {
                     console.log(JSON.stringify(data[i]))
-
+                    
                     if (data[i].fldSchoolUsername === localStorage.getItem('userName')) {
-                        var identifier = data[i].fldSchoolId;
+                        identifier = data[i].fldSchoolId;
 
-
-
-                    } else {
-                        alert('Could not find current user! Please login again.');
                     }
-
-
                 }
 
+                if (identifier != undefined) {
+
+                }
+            },
+            error: function (data) {
+                console.log(data.statusCode);
+            }
+        });
+    });
+
+    function CreateTeam() {
+        $.ajax({
+            type: 'POST',
+            url: '',
+            contentType: 'application/json',
+            data: { FldTeamName: , FldSchoolId: , FldSubjectCategory: ,  },
+            success: function (data) {
+                alert('Team created!')
 
 
             },
@@ -37,12 +57,5 @@
                 console.log(data.statusCode);
             }
         });
-
-        /*
-        $('#table').bootstrapTable({
-             data: data
-        });
-        */
-
-    });
+    }
 });
